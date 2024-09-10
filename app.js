e6566085afd17af041eaf5ee465c2ecedaa6544a
afquidangen <alyssafayequidangen@gmail.com>
2024-09-10 19:14:28 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// AND
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+console.log(x < y && y < z);
+console.log(x > y && y > z);
+
+//output: 
+// true 
+// false
+
+//OR
+
+let a = "Hello";
+let b = "World";
+let c = "Goodbye";
+
+console.log( a === b || a === c);
+console.log(a === b || b === c);
